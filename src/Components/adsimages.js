577415b5495ae1d8fa318d6cfc4f7fd64c96839d
paulanny <you@example.com>
2026-05-02 19:50:20 +0100

    updated some things

--- src/Components/adsimages.js
+++ src/Components/adsimages.js
@@ -2,27 +2,32 @@
 import pm1 from '../Assets/ADS/2.png'
 import pm2 from '../Assets/ADS/5.png'
 import pm3 from '../Assets/ADS/DML Flyer Design.jpg'
-import pm4 from '../Assets/ADS/Doris Tasty Bite Flyer.png'
-import pm5 from '../Assets/ADS/Layor Lingerie New.png'
-import pm6 from '../Assets/ADS/TiesMart FinalAsset 2@4x-8.png'
+import pm5 from '../Assets/ADS/ad 6.png'
+import pm6 from '../Assets/ADS/ad 3.png'
 import pm7 from '../Assets/ADS/ad1.png'
-import pm8 from '../Assets/ADS/ad10.png'
-import pm9 from '../Assets/ADS/ad3.png'
+import pm8 from '../Assets/ADS/TiesMart FinalAsset 2@4x-8.png'
 import pm10 from '../Assets/ADS/ad4.png'
-import pm11 from '../Assets/ADS/ad5.png'
+import pm11 from '../Assets/ADS/ad 6.png'
 import pm12 from '../Assets/ADS/ad6.png'
 import pm13 from '../Assets/ADS/ad7.png'
 import pm14 from '../Assets/ADS/ad8.png'
 import pm15 from '../Assets/ADS/ad9.png'
-import pm16 from '../Assets/ADS/ignite.jpg'
 import pm17 from '../Assets/ADS/luce.png'
 import pm18 from '../Assets/ADS/luceII.png'
 import pm19 from '../Assets/ADS/new ad.png'
 import pm20 from '../Assets/ADS/zad.png'
-
+import pm21 from '../Assets/ADS/kud7.png'
+import pm22 from '../Assets/ADS/adkuda.png'
+import pm23 from '../Assets/ADS/new ad.png'
+import pm24 from '../Assets/ADS/postinor1.png'
+import pm25 from '../Assets/ADS/postinor2.png'
+import pm26 from '../Assets/ADS/postinor3.png'
+import pm27 from '../Assets/ADS/postinor4.png'
+import pm28 from '../Assets/ADS/postinor5.png'
+import pm29 from '../Assets/ADS/postinor6.png'
 export const adPhotos = [
   {
-    src: pm1,
+    src: pm23,
     width: 1,
     height: 1,
     title: 'Hero Visual',
@@ -40,7 +45,7 @@ export const adPhotos = [
     title: 'Promo',
   },
   {
-    src: pm4,
+    src: pm20,
     width: 1,
     height: 1,
     title: 'Hero Visual',
@@ -70,7 +75,7 @@ export const adPhotos = [
     title: 'Promo',
   },
   {
-    src: pm9,
+    src: pm22,
     width: 1,
     height: 1,
     title: 'Hero Visual',
@@ -115,7 +120,7 @@ export const adPhotos = [
     title: 'Promo',
   },
   {
-    src: pm16,
+    src: pm21,
     width: 1,
     height: 1,
     title: 'Hero Visual',
@@ -147,26 +152,56 @@ export const adPhotos = [
     title: 'Hero Visual',
   },
   {
-    src: pm11,
+    src: pm29,
     width: 1,
     height: 1,
     title: 'Hero Visual',
   },
   {
-    src: pm19,
+    src: pm21,
     width: 1,
     height: 1,
     title: 'Hero Visual',
   },
 
   {
-    src: pm2,
+    src: pm22,
     width: 1,
     height: 1,
     title: 'Promo',
   },
   {
-    src: pm6,
+    src: pm25,
+    width: 1,
+    height: 1,
+    title: 'Promo',
+  },
+  {
+    src: pm24,
+    width: 1,
+    height: 1,
+    title: 'Promo',
+  },
+  {
+    src: pm1,
+    width: 1,
+    height: 1,
+    title: 'Promo',
+  },
+  {
+    src: pm26,
+    width: 1,
+    height: 1,
+    title: 'Promo',
+  },
+  {
+    src: pm27,
+    width: 1,
+    height: 1,
+    title: 'Promo',
+  },
+  {
+    src: pm28,
     width: 1,
     height: 1,
     title: 'Promo',
